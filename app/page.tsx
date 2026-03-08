018d96866caf5e9ad11857355855e9781ffc6e83
Hector Mendez <hectorm@evotechlv.io>
2026-03-08 13:15:54 -0700

    Updated main website, this is a preview

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,9 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Stats />
-      <About />
       <Services />
+      <About />
+      
       <Projects />
       <CTA />
       <Contact />
